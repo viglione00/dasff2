18bf7d9dde6a303d48c4f5b3b2f48e37f6240816
fpinto97 <francesco.pinto.1997@gmail.com>
2024-08-27 16:19:34 +0200

    n

---
+++ dasf2/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+function App() {
+  const [utenti, setUtenti] = useState([]);
+
+  useEffect(() => {
+    // Fare una richiesta al file PHP
+    fetch('http://dasff2.altervista.org/api/getTest.php')
+      .then(response => response.json())
+      .then(data => setUtenti(data))
+      .catch(error => console.error('Errore:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Elenco Utenti</h1>
+      <ul>
+        {utenti.map(utente => (
+          <li key={utente.test}>
+            {utente.nome} {utente.cognome}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
